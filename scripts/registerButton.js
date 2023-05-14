24fc29626ce3d3e5d4d1375ca0082e5d5e62619b
monjenahuel <nahuel.monje@estudiantes.unahur.edu.ar>
2023-05-13 23:50:33 -0300

    fix

--- scripts/registerButton.js
+++ scripts/registerButton.js
@@ -2,7 +2,7 @@ let registerButton = document.getElementById("registerButton")
 let currentURL = window.location.pathname;
 
 
-if(window.location.pathname === "/register.html"){
+if(window.location.pathname === "/CuiUnahur-P1/register.html"){
     registerButton.innerText = "Home"
     registerButton.addEventListener("click", () => {
         window.location.href = "/CuiUnahur-P1";
